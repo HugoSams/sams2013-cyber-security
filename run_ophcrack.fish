76722438d0862fd2b6f7db582ecbd66446699f11
Hugo Favila <hfavila@andrew.cmu.com>
2013-07-24 16:57:41 -0400

    adding ophcrack script

---
+++ run_ophcrack.fish
@@ -0,0 +1,16 @@
+--2013-07-24 16:53:59--  http://goo.gl/vawLnv
+Resolving goo.gl (goo.gl)... 74.125.228.68, 74.125.228.78, 74.125.228.65, ...
+Connecting to goo.gl (goo.gl)|74.125.228.68|:80... connected.
+HTTP request sent, awaiting response... 301 Moved Permanently
+Location: http://gs9671.sp.cs.cmu.edu/lectures/lecture3/run_ophcrack.fish [following]
+--2013-07-24 16:53:59--  http://gs9671.sp.cs.cmu.edu/lectures/lecture3/run_ophcrack.fish
+Resolving gs9671.sp.cs.cmu.edu (gs9671.sp.cs.cmu.edu)... 128.2.215.22
+Connecting to gs9671.sp.cs.cmu.edu (gs9671.sp.cs.cmu.edu)|128.2.215.22|:80... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 4287 (4.2K)
+Saving to: `vawLnv'
+
+     0K ....                                                  100%  793K=0.005s
+
+2013-07-24 16:53:59 (793 KB/s) - `vawLnv' saved [4287/4287]
+
